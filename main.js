c436edd269eff8b759b5f602a1f95ca45c50e242
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-21 14:20:48 +0200

    JS_Exercice_P4_5

---
+++ main.js
@@ -0,0 +1,27 @@
+const tableau = ['chat', 'chien', 'lapin', 'loup'];
+
+tableau.forEach(element => alert(element));
+
+// parcourir le tableau avec une boucle il existe différentes boucles
+
+// boucle "for" initialisation /condition/incrémentation 
+// const tableau = ['chat', 'chien', 'lapin', 'loup'];
+// for (var i = 0; i < tableau.length; i++) {
+    //on affiche le tableau dans le p par l'id
+    // document.getElementById('tab').innerHTML = tableau;
+    // console.log(tableau[i]);
+    // tableau[i]
+    // }
+    // la boucle infini, il faut faire attention il faut pouvoir sortir de la condition sinon cela ne s'arrete jamais!!
+    // const tableau = ['chat', 'chien', 'lapin', 'loup'];
+    // for (var i = 0; i < tableau.length; i+-) {
+    // document.getElementById('tab').innerHTML = tableau;
+    // console.log(tableau[i]);
+    // tableau[i]
+    // }
+
+
+
+
+
+
